39dffd7f5430c227728475c18ca6db2dfec817fb
Sara-2-1 <sara.aliebra@gmail.com>
2024-09-29 16:14:44 +0300

    Initial Commit

---
+++ Remindeme/RemindemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindemeApp.swift
+//  Remindeme
+//
+//  Created by Sarah ali  on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RemindemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
